8487f46e1fb502e024cd422978b2af115d768ae3
ijonk7 <jookeo.rizal@gmail.com>
2021-01-21 13:44:13 +0700

    add custom logs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/log', function () {
+    Log::info('This is an info log.');
+    Log::warning('This is a warning.');
+    Log::error('This is an error.');
 });
